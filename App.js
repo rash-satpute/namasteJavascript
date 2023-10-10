393b1892227a629b8cb9f0e3b257437ce640cca1
Reshma <reshma.satpute@digital.datamatics.com>
2023-10-10 13:28:53 +0530

    episode 1

--- App.js
+++ App.js
@@ -1,7 +1,30 @@
+/**
+ * <div id="parent">
+ *      <div id="child">
+ *          <h1>I am h1 tag</h1>
+ *          <h2>I am h2 tag </h2>
+ *      </div>
+ *      <div id="child">
+ *          <h1>I am h1 tag</h1>
+ *          <h2>I am h2 tag </h2>
+ *      </div>
+ * </div>
+ * ReactElement(Object) = HTML (Browser understand)
+ */
+
 const heading = React.createElement("h1",{id:"heading"},"Hello world reshma")
+
+
+
+const parent  = React.createElement ("div",{id:"parent"},
+                [React.createElement("div",{id:"child"},
+                [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]),
+                [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+
+            ])
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
-console.log("sdfkjdfkds")
-console.log("jkshdfkjsd")
-console.log("sdkjfhsdk")
-console.log("sdkfhkdsjf")
+root.render(parent)
+

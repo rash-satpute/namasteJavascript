e806910df0ffbc4e97c9cb43e0e48eaec7fa4e70
Reshma <reshma.satpute@digital.datamatics.com>
2025-01-06 22:16:10 +0530

    Reshma ke changes"

--- App.js
+++ App.js
@@ -20,6 +20,7 @@ const parent  = React.createElement ("div",{id:"parent"},
                 [React.createElement("div",{id:"child"},
                 [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am Mayur  tag")]),
                 [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am h2 tag")]
+                [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am Reshma tag")]
 
             ])
 
